b19edfbe15b6f6111386d405dbafc12f5dd335dc
Işıl Yapıcı <72387793+isilypici@users.noreply.github.com>
2022-01-08 20:40:16 +0300

    Update overlapgraph.py

--- overlapgraph.py
+++ overlapgraph.py
@@ -1,29 +1,32 @@
 import networkx as nx
-import matplotlib.pyplot as plt
-import random
+import matplotlib.pyplot as plt #grafiği görsel olarak da göstermek için 
+import random #TODO kaldırılabilir belli değil
 
-def compare(kmer1,kmer2, G):
+def compare(kmer1,kmer2, G): 
     
+    #difference = #difference büyüdükçe benzerlik azalacak 
     suffix = kmer1[1:]
-    prefix = kmer2[:-1]
-    
-    print("\t\tsuffix:", suffix, "prefix:", prefix)
+    prefix = kmer2[:-1] 
+    #TODO kaç adet suffix prefix eşleşmesi varsa o weight olmalı weight de bu sayı olmalı
+    #o kadar sayı kadar geriden başlamalı
     
     if(prefix == suffix):
-        print("\t\t\t\t\t\t\t\t\t+")
-        G.add_edge(kmer1,kmer2,weight = len(suffix))
-        
+        print("\t\tsuffix:", suffix, "prefix:", prefix , " + ")
+        G.add_edge(kmer1,kmer2,weight = len(suffix)) 
+        #TODO weight ayarlanmalı
+    else:
+        print("\t\tsuffix:", suffix, "prefix:", prefix)
     return G
 
 def reconstruct(G):
-    nodes = list(G)
-    edges = list(G.edges)
-    newGenome = []
+    nodes = list(G) #listede nodları tuttuk
+    edges = list(G.edges) #listede edgeleri tuttuk 0. gelen 1. gittiği nod
+    newGenome = [] 
     #print(len(nodes))
     #print(len(edges))
-    choosenNode = -1
+    choosenNode = -1 #rastgele sayı önemsiz
     counter = 0
-    while counter < len(nodes):
+    while counter < len(nodes): #bütün nodları gezerse gezdikten sonra devam etmesine gerek yok
         if (choosenNode != -1):
             randomStartNode = nodes.index(choosenNode)
         else:
@@ -41,7 +44,6 @@ def reconstruct(G):
         
 
 
-
 genomes = ["gtacgt","tacgta","acgtac","cgtacg","gtacga","tacgat"]
 temp = genomes.copy()
 G = nx.DiGraph()
@@ -53,20 +55,21 @@ while len(temp)>0:
     j = 0
     while j<len(genomes):
         kmer2 = genomes[j]
-        if(kmer1 == kmer2):
+        if(kmer1 == kmer2): #kendi kendini kontrol etmesin diye kendisini geçiyoruz 
             j+=1
             continue
         else:
-            print("\t",kmer1,"-->",kmer2)
+            print("\t",kmer1,"-->",kmer2) #diğer tüm kmer gnomeları karşılaştırarak node ve edge oluşturuyoruz
             compare(kmer1 ,kmer2 , G)
             j+=1
     
     
-    temp.pop(0)
+    temp.pop(0) #işlenmiş elemanı çıkartıyoruz 
     
 reconstruct(G)
 
-#output = nx.shortest_path(compare("tgtgtc","gtgtca"), weight='weight')
+#output = nx.shortest_path(compare("tgtgtc","gtgtca"), weight='weight') #2 nod araasında en kısa yolu bulmak için bir fonksiyon (kullanılmadı)
 
-subax1 = plt.subplot(121)
+#subax1 = plt.subplot(121)
+
 nx.draw(G, pos=nx.circular_layout(G), node_color='r', edge_color='b')
